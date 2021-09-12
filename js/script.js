1d89cb2fceaf21dc1e1f2a0509cf6758cce867fd
abay-2002 <akbar121202@gmail.com>
2021-09-12 22:06:40 +0800

    menemukan script untuk tambah dan kurang

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+
 function semuaMenu(){
     $.getJSON("data/menu.json", function (data) {
     let menu = data.menu;
@@ -9,29 +10,30 @@ function semuaMenu(){
                         <div class="card-body">
                         <h5 class="card-title">`+element.nama+`</h5>
                         <h6 class="card-subtitle text-muted mt-2">Rasa: `+element.rasa+`</h6>
+                        <h6 class="card-subtitle text-muted mt-2">id: `+element.addId+`</h6>
                         <h6 class="card-subtitle text-muted fs-6 mt-2">Harga: `+element.harga+`</h6>
                         <p class="card-text mt-2">`+element.deskripsi+`</p>
-                        <a href="" class="btn btn-primary my-auto mx-auto tambah">+</a>
-                        <a href="" class="btn btn-primary my-auto mx-auto kurang">-</a>
+                        <a href="" class="btn btn-primary my-auto mx-auto tambah" id="`+element.addId+`">+</a>
+                        <a href="" class="btn btn-primary my-auto mx-auto kurang" id="R`+element.removeId+`">-</a>
                         </div>
                     </div>
                 </div>`
             );
-        });
-        // pengulangan dan event untuk class tambah untuk dijadikan cart
-        let tambah = $('.tambah');
-        let nilaiAwal = 0;
-        for(i=0; i<tambah.length; i++){
-            let tambahItem = $('.tambah')[i];
-            $(tambahItem).on('click',function(event){
+            let idTambah = element.addId;
+            $(`#`+idTambah+``).on('click',function(event){
                 event.preventDefault();
-                nilaiAwal += 1;
-                console.log(nilaiAwal);
+                alert('tambah');
             });
-        }
+
+            let idKurang = element.removeId;
+            $(`#R`+idKurang+``).on('click',function(event){
+                event.preventDefault();
+                alert('kurang');
+            });
+
+        });
     });
 }
-
 
 semuaMenu();
 
@@ -71,7 +73,6 @@ $('.nav-link').on('click',function(){
         let content = "";
 
         $.each(menu, function(i, element){
-            console.log(element.kategori);
             if(element.kategori == (kategori.toLowerCase())){
                 content += `
                 <div class="col-sm-4 mx-auto my-4">
@@ -80,19 +81,29 @@ $('.nav-link').on('click',function(){
                         <div class="card-body">
                         <h5 class="card-title">`+element.nama+`</h5>
                         <h6 class="card-subtitle text-muted mt-2">Rasa: `+element.rasa+`</h6>
+                        <h6 class="card-subtitle text-muted mt-2">id: `+element.addId+`</h6>
                         <h6 class="card-subtitle text-muted fs-6 mt-2">Harga: `+element.harga+`</h6>
                         <p class="card-text mt-2">`+element.deskripsi+`</p>
-                        <a href="" class="btn btn-primary my-auto mx-auto tambah">+</a>
-                        <a href="" class="btn btn-primary my-auto mx-auto kurang">-</a>
+                        <a href="" class="btn btn-primary my-auto mx-auto tambah" id="`+element.addId+`">+</a>
+                        <a href="" class="btn btn-primary my-auto mx-auto kurang" id="R`+element.removeId+`">-</a>
                         </div>
                     </div>
                 </div>
                 `;
-            }else{
-                backup();
-            };
+            }
+            $("#list-menu").html(content);
+            let idTambah = element.addId;
+            $(`#`+idTambah+``).on('click',function(event){
+                event.preventDefault();
+                alert('tambah');
+            });
+
+            let idKurang = element.removeId;
+            $(`#R`+idKurang+``).on('click',function(event){
+                event.preventDefault();
+                alert('kurang');
+            });
         });
-        $("#list-menu").html(content);
     });
 
     // fetch JSON data element object JSON
